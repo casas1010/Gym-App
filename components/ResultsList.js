b93f2a6ad5ade42027913c5a3eb7b145a15c5fc2
juan <jcasasmail@gmail.com>
2020-08-12 17:43:14 -0400

    search bar complete

--- components/ResultsList.js
+++ components/ResultsList.js
@@ -1,4 +1,61 @@
-import React, { useEffect, useRef } from "react";
+// import React, { useEffect, useRef } from "react";
+// import {
+//   View,
+//   Text,
+//   StyleSheet,
+//   FlatList,
+//   TouchableOpacity,
+// } from "react-native";
+// // import { withNavigation } from 'react-navigation';
+// // import ResultsDetail from './ResultsDetail';
+
+// const ResultsList = ({ title, results=[], navigation }) => {
+//   const isInitialMount = useRef(true);
+
+//   useEffect(() => {
+//     if (isInitialMount.current) {
+//       isInitialMount.current = false;
+//     } else {
+//       // console.log('results:  ',results.exerciseNames )
+//     }
+//   }, [results]);
+
+//   if (!results.length) {
+//     return null;
+//   }
+
+//   return (
+//     <View style={styles.container}>
+//       <Text style={styles.title}>{title}</Text>
+//       <FlatList
+//         horizontal
+//         showsHorizontalScrollIndicator={false}
+//         data={results}
+//         keyExtractor={(result) => result.exerciseName}
+//         renderItem={(item) => {
+//           console.log(item)
+//           return <Text>{item.exerciseName}</Text>;
+//         }}
+//       />
+//     </View>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   title: {
+//     fontSize: 18,
+//     fontWeight: "bold",
+//     marginLeft: 15,
+//     marginBottom: 5,
+//   },
+//   container: {
+//     marginBottom: 10,
+//   },
+// });
+
+// export default ResultsList;
+
+import React, { useEffect } from "react";
 import {
   View,
   Text,
@@ -6,19 +63,20 @@ import {
   FlatList,
   TouchableOpacity,
 } from "react-native";
-// import { withNavigation } from 'react-navigation';
-// import ResultsDetail from './ResultsDetail';
 
-const ResultsList = ({ title, results=[], navigation }) => {
-  const isInitialMount = useRef(true);
+{
+  /* <TouchableOpacity
+onPress={() =>
+  navigation.navigate(ite)
+}
+>
+</TouchableOpacity> */
+}
 
+const ResultsList = ({ results, navigation }) => {
   useEffect(() => {
-    if (isInitialMount.current) {
-      isInitialMount.current = false;
-    } else {
-      // console.log('results:  ',results.exerciseNames )
-    }
-  }, [results]);
+    // console.log()
+  });
 
   if (!results.length) {
     return null;
@@ -26,15 +84,17 @@ const ResultsList = ({ title, results=[], navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{title}</Text>
       <FlatList
         horizontal
         showsHorizontalScrollIndicator={false}
         data={results}
         keyExtractor={(result) => result.exerciseName}
-        renderItem={(item) => {
-          console.log(item)
-          return <Text>{item.exerciseName}</Text>;
+        renderItem={({ item }) => {
+          return (
+            <View>
+              <Text>{item.exerciseName}</Text>
+            </View>
+          );
         }}
       />
     </View>
--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,3 +1,126 @@
+// import React, { useState, useEffect, useRef } from "react";
+// import {
+//   View,
+//   Text,
+//   StyleSheet,
+//   ScrollView,
+//   FlatList,
+//   SafeAreaView,
+//   Dimensions,
+//   TouchableOpacity,
+//   Image,
+// } from "react-native";
+// import { Button } from "react-native-elements";
+
+// import { connect } from "react-redux";
+// import * as actions from "../actions/index";
+
+// import SearchBar from "../components/SearchBar";
+// import useResults from "../hooks/useResults";
+// import ResultsList from "../components/ResultsList";
+// import ListCard from "../components/ListCard";
+// import BackGround from "../components/BackGround";
+// const SCREEN_WIDTH = Dimensions.get("window").width;
+// const BUTTON_WIDTH = SCREEN_WIDTH * 0.3;
+
+// // PULL ALLL IMAGES FROM HERE: https://www.britannica.com/science/human-muscle-system/The-abdomen
+
+// const SearchScreen = (props) => {
+//   const [term, setTerm] = useState("");
+
+//   const openCardDetails = (name) => {
+//     console.log(`card ${name} has been clicked!`);
+//     props.navigation.navigate("details", {
+//       name: name,
+//     });
+//   };
+
+//   const filterExercisesByName = (name) => {
+//     name=name.toLowerCase()
+//     return props.exercises.filter((result) => {
+//       return result.exerciseName.toLowerCase() == name;
+//     });
+//   };
+
+//   return (
+//     <BackGround>
+//       <SafeAreaView>
+//         <SearchBar
+//           term={term}
+//           onTermChange={setTerm}
+//           onTermSubmit={() => {
+//             console.log(`term searched is ${term}`);
+//             console.log( filterExercisesByName('ab Rollout'))
+//           }}
+//         />
+//         <FlatList
+//           horizontal={false}
+//           style={styles.list}
+//           keyExtractor={(element) => element.exerciseName}
+//           data={props.exercises}
+//           renderItem={(element) => {
+//             // console.log('element.item:  ',element.item.exerciseName);
+//             return (
+//               <ListCard
+//                 imageURL={element.item.anatomyPicture}
+//                 name={element.item.exerciseName}
+//                 callBack={openCardDetails}
+//               />
+//             );
+//           }}
+//         />
+//       </SafeAreaView>
+//     </BackGround>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   list: {
+//     paddingLeft: 15,
+//     paddingTop: 20,
+//     paddingRight: 15,
+//   },
+// });
+
+// SearchScreen.navigationOptions = (props) => ({
+//   title: "",
+//   headerStyle: {
+//     backgroundColor: "#000c23",
+//     shadowRadius: 0,
+//     shadowOffset: {
+//       height: 0,
+//     },
+//   },
+
+//   headerRight: () => (
+//     <Button //RIGHT BUTTON
+//       title="  . . .     "
+//       buttonStyle={{ width: BUTTON_WIDTH, backgroundColor: "#000c23" }}
+//       textStyle={{ color: "white", fontSize: 100, fontWeight: "bold" }}
+//       onPress={() => {
+//         console.log("3 buttons presses");
+//       }}
+//     />
+//   ),
+//   headerLeft: () => (
+//     <Button // LEFT BUTTON
+//       title="  + New"
+//       textStyle={{ color: "white", fontSize: 100, fontWeight: "bold" }}
+//       buttonStyle={{ backgroundColor: "#000c23", width: BUTTON_WIDTH }}
+//       onPress={() => {
+//         console.log("+ New pressed");
+//       }}
+//     />
+//   ),
+// });
+
+// // export default SearchScreen;
+// function mapStateToProps({ auth, exercise }) {
+//   return { exercises: exercise.exercises };
+// }
+
+// export default connect(mapStateToProps, actions)(SearchScreen);
+
 import React, { useState, useEffect, useRef } from "react";
 import {
   View,
@@ -17,16 +140,17 @@ import * as actions from "../actions/index";
 
 import SearchBar from "../components/SearchBar";
 import useResults from "../hooks/useResults";
-import ResultsList from "../components/ResultsList";
 import ListCard from "../components/ListCard";
 import BackGround from "../components/BackGround";
+import ResultsList from "../components/ResultsList";
+
 const SCREEN_WIDTH = Dimensions.get("window").width;
 const BUTTON_WIDTH = SCREEN_WIDTH * 0.3;
 
 // PULL ALLL IMAGES FROM HERE: https://www.britannica.com/science/human-muscle-system/The-abdomen
 
 const SearchScreen = (props) => {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("Ab Rollout");
 
   const openCardDetails = (name) => {
     console.log(`card ${name} has been clicked!`);
@@ -36,8 +160,9 @@ const SearchScreen = (props) => {
   };
 
   const filterExercisesByName = (name) => {
+    name = name.toLowerCase();
     return props.exercises.filter((result) => {
-      return result.exerciseName.toLowerCase() == name.toLowerCase();
+      return result.exerciseName.toLowerCase() == name;
     });
   };
 
@@ -49,25 +174,10 @@ const SearchScreen = (props) => {
           onTermChange={setTerm}
           onTermSubmit={() => {
             console.log(`term searched is ${term}`);
-            console.log( filterExercisesByName('Ab Rollout'))
-          }}
+            console.log(filterExercisesByName(term));
+          }} 
         />
-        <FlatList
-          horizontal={false}
-          style={styles.list}
-          keyExtractor={(element) => element.exerciseName}
-          data={props.exercises}
-          renderItem={(element) => {
-            // console.log('element.item:  ',element.item.exerciseName);
-            return (
-              <ListCard
-                imageURL={element.item.anatomyPicture}
-                name={element.item.exerciseName}
-                callBack={openCardDetails}
-              />
-            );
-          }}
-        />
+        <ResultsList results={filterExercisesByName(term)} navigation={props.navigation} />
       </SafeAreaView>
     </BackGround>
   );
